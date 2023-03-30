6bb4a62541163eff6712f13e22b8469af1173e08
PhakornKiong <phakorn214@gmail.com>
2023-03-31 04:20:17 +0800

    update-result

--- dsl/workflow/activity.go
+++ dsl/workflow/activity.go
@@ -14,23 +14,23 @@ type SampleActivities struct {
 func (a *SampleActivities) SampleActivity1(ctx context.Context, input interface{}) (string, error) {
 	name := activity.GetInfo(ctx).ActivityType.Name
 	rlog.Info(fmt.Sprintf("Run %s with input %v \n", name, input))
-	return fmt.Sprint(input), nil
+	return "Result_" + name + " " + fmt.Sprint(input), nil
 }
 
 func (a *SampleActivities) SampleActivity2(ctx context.Context, input interface{}) (string, error) {
 	name := activity.GetInfo(ctx).ActivityType.Name
 	rlog.Info(fmt.Sprintf("Run %s with input %v \n", name, input))
-	return "Result_" + name, nil
+	return "Result_" + name + " " + fmt.Sprint(input), nil
 }
 
 func (a *SampleActivities) SampleActivity3(ctx context.Context, input interface{}) (string, error) {
 	name := activity.GetInfo(ctx).ActivityType.Name
 	rlog.Info(fmt.Sprintf("Run %s with input %v \n", name, input))
-	return "Result_" + name, nil
+	return "Result_" + name + " " + fmt.Sprint(input), nil
 }
 
 func (a *SampleActivities) SampleActivity4(ctx context.Context, input interface{}) (string, error) {
 	name := activity.GetInfo(ctx).ActivityType.Name
 	rlog.Info(fmt.Sprintf("Run %s with input %v \n", name, input))
-	return "Result_" + name, nil
+	return "Result_" + name + " " + fmt.Sprint(input), nil
 }
